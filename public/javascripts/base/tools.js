23f43665e9fc579c5d5120201b7194e6a6b5f6e4
Shaw <wenwu.shao@gmail.com>
2014-07-12 16:20:08 +0800

    Login Page
    Login Page

---
+++ public/javascripts/base/tools.js
@@ -0,0 +1,217 @@
+/**
+ * Coder: Sean
+ * Date: 14-7-12
+ * Time: 下午3:16
+ */
+if (window.MNK == null) {
+    window.MNK = {};
+}
+(function () {
+    $.extend(MNK, {
+        isEmpty: function (value) {
+            // 判断是否为空值
+            var result = value === "" || value === null || value === undefined;
+            return result;
+        },
+        /**
+         * 封装过的jQuery.ajax()函数，对data参数做了中日韩文编码处理
+         * @static
+         * @param {Object} options ajax参数
+         */
+        ajax: function (options) {
+            if (options) {
+                options.data = MNK.cjkEncodeDO(options.data);
+            }
+
+            $.ajax(options);
+        },
+        cjkEncode: function (text) {
+            if (typeof text !== 'string') {
+                return text;
+            }
+            var newText = "";
+            for (var i = 0; i < text.length; i++) {
+                var code = text.charCodeAt(i);
+                if (code >= 128 || code === 91 || code === 93) {//91 is "[", 93 is "]".
+                    newText += "[" + code.toString(16) + "]";
+                } else {
+                    newText += text.charAt(i);
+                }
+            }
+            return newText
+        },
+        cjkDecode: function (text) {
+            if (text == null) {
+                return "";
+            }
+            if (!isNaN(text) || text.indexOf('[') == -1) {
+                return text;
+            }
+            var newText = "";
+            for (var i = 0; i < text.length; i++) {
+                var ch = text.charAt(i);
+                if (ch == '[') {
+                    var rightIdx = text.indexOf(']', i + 1);
+                    if (rightIdx > i + 1) {
+                        var subText = text.substring(i + 1, rightIdx);
+                        //james：主要是考虑[CDATA[]]这样的值的出现
+                        if (subText.length > 0) {
+                            ch = String.fromCharCode(eval("0x" + subText));
+                        }
+
+                        i = rightIdx;
+                    }
+                }
+                newText += ch;
+            }
+            return newText;
+        },
+        /**
+         * 对指定的键值对对象做中日韩文编码处理
+         * @static
+         * @param {Object} o 键值对对象
+         * @return {Object} 经过了中日韩文编码处理的键值对
+         */
+        cjkEncodeDO: function (o) {
+            if ($.isPlainObject(o)) {
+                var result = {};
+                $.each(o, function (k, v) {
+                    if (!(typeof v == "string")) {
+                        v = MNK.jsonEncode(v);
+                    }
+                    //wei:bug 43338，如果key是中文，cjkencode后o的长度就加了1，ie9以下版本死循环，所以新建对象result。
+                    k = MNK.cjkEncode(k);
+                    result[k] = MNK.cjkEncode(v);
+                });
+                return result;
+            }
+            return o;
+        },
+        jsonEncode: function (o) {
+            var useHasOwn = {}.hasOwnProperty ? true : false;
+            var m = {
+                "\b": '\\b',
+                "\t": '\\t',
+                "\n": '\\n',
+                "\f": '\\f',
+                "\r": '\\r',
+                '"': '\\"',
+                "\\": '\\\\'
+            };
+            var encodeString = function (s) {
+                if (/["\\\x00-\x1f]/.test(s)) {
+                    return '"' + s.replace(/([\x00-\x1f\\"])/g, function (a, b) {
+                        var c = m[b];
+                        if (c) {
+                            return c;
+                        }
+                        c = b.charCodeAt();
+                        return "\\u00" +
+                            Math.floor(c / 16).toString(16) +
+                            (c % 16).toString(16);
+                    }) + '"';
+                }
+                return '"' + s + '"';
+            };
+            var encodeArray = function (o) {
+                var a = ["["], b, i, l = o.length, v;
+                for (i = 0; i < l; i += 1) {
+                    v = o[i];
+                    switch (typeof v) {
+                        case "undefined":
+                        case "function":
+                        case "unknown":
+                            break;
+                        default:
+                            if (b) {
+                                a.push(',');
+                            }
+                            a.push(v === null ? "null" : MNK.jsonEncode(v));
+                            b = true;
+                    }
+                }
+                a.push("]");
+                return a.join("");
+            };
+            if (typeof o == "undefined" || o === null) {
+                return "null";
+            } else if ($.isArray(o)) {
+                return encodeArray(o);
+            } else if (o instanceof Date) {
+                return MNK.jsonEncode({
+                    __time__: o.getTime()
+                })
+            } else if (typeof o == "string") {
+                return encodeString(o);
+            } else if (typeof o == "number") {
+                return isFinite(o) ? String(o) : "null";
+            } else if (typeof o == "boolean") {
+                return String(o);
+            } else if ($.isFunction(o)) {
+                return String(o);
+            } else {
+                var a = ["{"], b, i, v;
+                for (i in o) {
+                    if (!useHasOwn || o.hasOwnProperty(i)) {
+                        v = o[i];
+                        switch (typeof v) {
+                            case "undefined":
+                            case "unknown":
+                                break;
+                            default:
+                                if (b) {
+                                    a.push(',');
+                                }
+                                a.push(MNK.jsonEncode(i), ":",
+                                        v === null ? "null" : MNK.jsonEncode(v));
+                                b = true;
+                        }
+                    }
+                }
+                a.push("}");
+                return a.join("");
+            }
+        },
+        jsonDecode: function (text) {
+            try {
+                var jo = $.parseJSON(text);
+                if (jo == null) {
+                    jo = {};
+                }
+            } catch (e) {
+                try {
+                    jo = new Function("return " + text)() || {};
+                } catch (e) {
+                }
+                if (jo == null) {
+                    jo = [];
+                }
+            }
+            if(!MNK._hasDateInJson(text)){
+                return jo;
+            }
+            return (function (o) {
+                if (typeof o === "string") {
+                    return o;
+                }
+                if (o && o.__time__ != null) {
+                    return new Date(o.__time__);
+                }
+                for (var a in o) {
+                    if (o[a] == o || typeof o[a] == 'object' || $.isFunction(o[a])) {
+                        break;
+                    }
+                    o[a] = arguments.callee(o[a]);
+                }
+
+                return o;
+            })(jo);
+        },
+        _hasDateInJson : function(json){
+            if(!json || typeof json !== "string"){
+                return false;
+            }
+            return json.indexOf("__time__")!=-1;
+        }
+    });
+})();
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -7,9 +7,9 @@ $(function() {
     //报错蒙板
     var $mask = $('<div class="login-errmask"/>');
     //用户名
-    var $username = $('input.login-username').attr("placeholder", FR.i18nText('Username')).attr('title',FR.i18nText('Username'));
+    var $username = $('input.login-username').attr("placeholder", $.i18n.prop('LOGIN-Username')).attr('title',$.i18n.prop('Username'));
     //密码
-    var $password = $('input.login-password').attr("placeholder", FR.i18nText('Password')).attr('title',FR.i18nText('Password'));
+    var $password = $('input.login-password').attr("placeholder", $.i18n.prop('LOGIN-Password')).attr('title',$.i18n.prop('Password'));
     $('input').focus(function(){
         $(this).parent().addClass('login-input-focus');
         $mask.remove();
@@ -17,13 +17,13 @@ $(function() {
             $(this).parent().removeClass('login-input-focus');
         });
     //是否保持登录状态
-    var $keep = $('span.login-remember').text(FR.i18nText('Privilege-Keep_Login_State')).click(
+    var $keep = $('span.LOGIN-Keep_State').text($.i18n.prop('LOGIN-Keep_State')).click(
         function(){
-            $(this).toggleClass('login-remember-selected');
+            $(this).toggleClass('LOGIN-Keep_State-selected');
         }
     );
     //登录按钮
-    $('a').text(FR.i18nText('Sign_In')).click(
+    $('a').text($.i18n.prop('LOGIN-Login')).click(
         function(){
             signIN();
         }
@@ -34,37 +34,6 @@ $(function() {
             signIN();
         }
     });
-    /**
-     * 初始化FS的登录背景图片
-     */
-    var initBackgroundImage = function () {
-        var self = this;
-        var ran = new Date().getTime() + "" + (Math.random() * 1000);
-        FR.ajax({
-            url: FR.servletURL + "?op=fs_load&cmd=getLoginImageID&_ran=" + ran,
-            complete: function (res, status) {
-                if (status == 'success') {
-                    var loginID = FR.jsonDecode(res.responseText);
-                    self.loginImgID = loginID.id;
-                    var url = FR.servletURL
-                        + ((self.loginImgID && self.loginImgID != 'null') ? ('?op=fr_attach&cmd=ah_image&id=' + self.loginImgID + '&isAdjust=false') : '?op=resource&resource=/com/fr/base/images/oem/signin.png');
-                    if ($('body').length > 0) {
-                        $('body').css({
-                            background: 'url(' + url + ') no-repeat',
-                            width: '100%',
-                            height: '100%',
-                            overflow: 'hidden'
-                        });
-                        var offset = $('#login-scalebg').offset();
-                        $('#login-scalebg').css({
-                            background: 'url(' + url + ') no-repeat -'+offset.left+'px -'+offset.top+'px',
-                            overflow: 'hidden'
-                        });
-                    }
-                }
-            }
-        })
-    };
 
     var showErrorMsg = function($pos, msg){
         $mask.hide().insertAfter($pos).text(msg);
@@ -79,18 +48,18 @@ $(function() {
         var user = $username.val();
         var pw = $password.val();
         //用户名为空
-        if(FR.isEmpty(user)){
-            showErrorMsg($username,FR.i18nText('Privilege-User_Can_Not_Be_Null'));
+        if(MNK.isEmpty(user)){
+            showErrorMsg($username,$.i18n.prop('Privilege-User_Can_Not_Be_Null'));
             return;
         }
         //密码为空
-        if(FR.isEmpty(pw)){
-            showErrorMsg($password,FR.i18nText('Privilege-Password_Can_Not_Be_Null'));
+        if(MNK.isEmpty(pw)){
+            showErrorMsg($password,$.i18n.prop('Privilege-Password_Can_Not_Be_Null'));
             return;
         }
-        FR.ajax({
-            url : FR.servletURL + '?op=fs_load&cmd=login',
-            data : FR.cjkEncodeDO({
+        MNK.ajax({
+            url : MNK.servletURL + '?op=fs_load&cmd=login',
+            data : MNK.cjkEncodeDO({
                 fsusername : encodeURIComponent(user),
                 fspassword : encodeURIComponent(pw),
                 fsremember : $keep.hasClass('selected')
@@ -98,30 +67,22 @@ $(function() {
             type : 'POST',
             async : false,
             error : function() {
-                FR.Msg.toast("Error!");
+                alert("Error!");
             },
             complete : function(res, status) {
                 if (res.responseText == "") {
-                    showErrorMsg($username,FR.i18nText('Privilege-Authentication_failed'));
+                    showErrorMsg($username,$.i18n.prop('Privilege-Authentication_failed'));
                     return;
                 }
-                var signResult = FR.jsonDecode(res.responseText);
+                var signResult = MNK.jsonDecode(res.responseText);
                 if (signResult.fail) {
                     //用户名和密码不匹配
-                    showErrorMsg($username,FR.i18nText('Privilege-Name_Not_Match_Password'));
+                    showErrorMsg($username,$.i18n.prop('Privilege-Name_Not_Match_Password'));
                 } else if (signResult.url) {
                     window.location.href = signResult.url;
                 }
             }
         });
     };
-    initBackgroundImage();
     $username.focus();
-    $(window).resize(function(){
-        var offset = $('#login-scalebg').offset();
-        $('#login-scalebg').css({
-            'background-position-x': -offset.left,
-            'background-position-y': -offset.top
-        });
-    });
 });
